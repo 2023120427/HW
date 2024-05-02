13aa33725e362e13917934d98d852eb631b3268f
2023120427 <skssk12356@nate.com>
2024-05-03 03:50:54 +0900

    copyobject복사

---
+++ ex1-13.js
@@ -0,0 +1,13 @@
+  var user = {
+    name: 'Jaenam',
+    gender: 'male',
+  };
+  
+  var user2 = copyObject(user);
+  user2.name = 'Jung';
+  
+  if (user !== user2) {
+    console.log('유저 정보가 변경되었습니다.');
+  }
+  console.log(user.name, user2.name);
+  console.log(user === user2);
