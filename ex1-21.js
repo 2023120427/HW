9482c4aecbabbcea3ef60b6afb7e20c654494f3e
2023120427 <skssk12356@nate.com>
2024-05-03 03:52:40 +0900

    빈 요소와 배열의 순회

---
+++ ex1-21.js
@@ -0,0 +1,15 @@
+var arr1 = [undefined, 1];
+var arr2 = [];
+arr2[1] = 1;
+
+arr1.forEach(function(v, i) {console.log(v, i);});
+arr2.forEach(function(v, i) {console.log(v, i);});
+
+arr1.map(function(v, i) {return v + i;});
+arr2.map(function(v, i) {return v + i;});
+
+arr1.filter(function(v) {return !v;});
+arr2.filter(function(v) {return !v;});
+
+arr1.reduce(function(p, c, i) {return p + c + i;}, '');
+arr2.reduce(function(p, c, i) {return p + c + i;}, '');
