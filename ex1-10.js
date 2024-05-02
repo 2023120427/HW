de0628fb33455203b543d610341c10cc4ece30c8
2023120427 <skssk12356@nate.com>
2024-05-03 03:50:24 +0900

    가변성 문제점

---
+++ ex1-10.js
@@ -0,0 +1,18 @@
+var user={
+    name:'jaenam',
+    gender:'male'
+};
+
+var changeName=function(user, newName){
+    var newName=user;
+    newUser.name=newName;
+    return newUser;
+};
+
+var user2=changeName(user,'Jung');
+
+if(user !== user2){
+    console.log('유저 정보가 변경되었습니다.');
+}
+console.log(user.name, user2.name);
+console.log(user===user2);
