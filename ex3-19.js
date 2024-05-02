815238be2918d459cfe02903ef89972c07074a80
2023120427 <skssk12356@nate.com>
2024-05-03 03:59:10 +0900

    call/apply 메서드 활용3

---
+++ ex3-19.js
@@ -0,0 +1,19 @@
+var str = 'abc def';
+
+Array.prototype.push.call(str, ', pushed string');
+Array.prototype.concat.call(str, 'string');
+Array.prototype.every.call(str, function(char) {return char !== ' ';});
+
+Array.prototype.some.call(str, function(char) {return char === ' ';});
+
+var newArr = Array.prototype.map.call(str, function(char) {return char + '!';
+});
+console.log(newArr);
+
+var newStr = Array.prototype.reduce.apply(str, [
+  function(string, char, i) {
+    return string + char + i;
+  },
+  '',
+]);
+console.log(newStr);
